b031d86176397a3ada44e16e5b549f2b37221fea
Max Espinosa <maxespinosa@Maxs-MacBook-Air.local>
2023-06-13 11:03:31 -0400

    UPDATED

--- mysql-homework.sql
+++ mysql-homework.sql
@@ -2,17 +2,16 @@ CREATE TABLE movies (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     movie_id VARCHAR(6) NOT NULL,
 
-    movie_name VARCHAR(50),
-    publish_year INT(4),
+    movie_name VARCHAR(200),
+    publish_year INT,
     genre VARCHAR(50),
-    duration INT(3),
-    director VARCHAR(30),
-    main_actor VARCHAR(30),
-    total_revenue INT(50),
-    cover VARCHAR(30000),
+    duration INT,
+    director VARCHAR(100),
+    main_actor VARCHAR(100),
+    total_revenue INT,
+    cover VARCHAR(10000),
     rating VARCHAR(10),
-    producer VARCHAR(50),
+    producer VARCHAR(100),
     budget INT(30),
-
-    created_at DATETIME
+    
 );
